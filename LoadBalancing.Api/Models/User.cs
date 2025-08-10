fd504a76c06de31e3777472a00b717f5646d3bd2
hassa <hassa@LAPTOP-U03CN6LR>
2025-08-11 00:48:05 +0330

    add apis for adduser and getallusers

---
+++ LoadBalancing.Api/Models/User.cs
@@ -0,0 +1,7 @@
+﻿internal sealed class User
+{
+    public int Id { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public DateTime DateOfBirth { get; init; }
+}
--- LoadBalancing.Api/Program.cs
+++ LoadBalancing.Api/Program.cs
@@ -1,16 +1,99 @@
+﻿using Bogus;
+using Dapper;
+using Npgsql;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var connectionString = builder.Configuration.GetConnectionString("Database");
+var dataSource = new NpgsqlDataSourceBuilder(connectionString).Build();
+builder.Services.AddSingleton(dataSource);
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.MapOpenApi();
+app.MapScalarApiReference();
 
-if (app.Environment.IsDevelopment())
+await EnsureUserTable(app.Services);
+
+app.MapPost("addUser", async (NpgsqlDataSource source) =>
 {
-    app.MapOpenApi();
-}
+    var faker = new Faker();
 
+    await using var connection = await source.OpenConnectionAsync();
+
+    var userId = await connection.ExecuteScalarAsync<int>(
+        """
+        INSERT INTO users (first_name, last_name, date_of_birth)
+        VALUES (@FirstName, @LastName, @DateOfBirth)
+        RETURNING id;
+        """,
+        new User
+        {
+            FirstName = faker.Name.FirstName(),
+            LastName = faker.Name.LastName(),
+            DateOfBirth = faker.Date.Past(20).ToUniversalTime()
+        });
+
+    connection.Close();
+
+    return Results.Ok(new { id = userId });
+});
+
+app.MapGet("getAllUsers", async (NpgsqlDataSource source) =>
+{
+    await using var connection = await source.OpenConnectionAsync();
+    var users = await connection.QueryAsync<User>(
+        """
+        SELECT
+            id AS Id,
+            first_name AS FirstName,
+            last_name AS LastName,
+            date_of_birth AS DateOfBirth
+        FROM users
+        ORDER BY id;
+        """
+    );
+
+    return Results.Ok(users);
+});
 
 app.Run();
+
+
+#region EnsureUserTable
+async Task EnsureUserTable(IServiceProvider services)
+{
+    await using var scope = services.CreateAsyncScope();
+    var dataSource = scope.ServiceProvider.GetRequiredService<NpgsqlDataSource>();
+
+    await using var connection = await dataSource.OpenConnectionAsync();
+
+    var exists = await connection.ExecuteScalarAsync<bool>(
+        """
+        SELECT EXISTS (
+            SELECT FROM information_schema.tables 
+            WHERE table_schema = 'public' 
+            AND table_name = 'users'
+        );
+        """
+    );
+
+    if (!exists)
+    {
+        await connection.ExecuteAsync(
+            """
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                date_of_birth TIMESTAMP NOT NULL
+            );
+            """
+        );
+    }
+
+}
+#endregion
