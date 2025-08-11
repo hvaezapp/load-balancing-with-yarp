0b6f2a4485b1d8193e5172228d5bea04e3fc461e
hassa <hassa@LAPTOP-U03CN6LR>
2025-08-11 13:18:17 +0330

    add scalar for dev env

--- LoadBalancing.Api/Program.cs
+++ LoadBalancing.Api/Program.cs
@@ -1,13 +1,18 @@
 ﻿using LoadBalancing.Api.Bootstraper;
 using LoadBalancing.Api.Endpoints;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.RegisterPostgresSql();
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 app.MapUserEndpoints();
+app.MapScalarApiReference();
+
 
 app.Run();
 
