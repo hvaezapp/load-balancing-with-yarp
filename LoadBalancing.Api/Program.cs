646e744fd3457d59607ee11a4bb88dc054ea8dba
hassa <hassa@LAPTOP-U03CN6LR>
2025-08-11 12:39:12 +0330

    fix docker compose and add create database in default mode postgres

--- LoadBalancing.Api/Program.cs
+++ LoadBalancing.Api/Program.cs
@@ -1,24 +1,17 @@
 ﻿using Bogus;
 using Dapper;
 using Npgsql;
-using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("Database");
 var dataSource = new NpgsqlDataSourceBuilder(connectionString).Build();
-builder.Services.AddSingleton(dataSource);
 
-builder.Services.AddOpenApi();
+builder.Services.AddSingleton(dataSource);
 
 var app = builder.Build();
 
-app.MapOpenApi();
-app.MapScalarApiReference();
-
-await EnsureUserTable(app.Services);
-
-app.MapPost("addUser", async (NpgsqlDataSource source) =>
+app.MapPost("addFakeUser", async (NpgsqlDataSource source) =>
 {
     var faker = new Faker();
 
@@ -42,10 +35,11 @@ app.MapPost("addUser", async (NpgsqlDataSource source) =>
     return Results.Ok(new { id = userId });
 });
 
-app.MapGet("getAllUsers", async (NpgsqlDataSource source) =>
+app.MapGet("users/{id}", async (int id, NpgsqlDataSource source) =>
 {
     await using var connection = await source.OpenConnectionAsync();
-    var users = await connection.QueryAsync<User>(
+
+    var user = await connection.QuerySingleAsync<User>(
         """
         SELECT
             id AS Id,
@@ -53,47 +47,15 @@ app.MapGet("getAllUsers", async (NpgsqlDataSource source) =>
             last_name AS LastName,
             date_of_birth AS DateOfBirth
         FROM users
-        ORDER BY id;
-        """
-    );
+        WHERE id = @UserId
+        """,
+        new { UserId = id });
 
-    return Results.Ok(users);
+    connection.Close();
+
+    return Results.Ok(user);
+
 });
 
 app.Run();
 
-
-#region EnsureUserTable
-static async Task EnsureUserTable(IServiceProvider services)
-{
-    await using var scope = services.CreateAsyncScope();
-    var dataSource = scope.ServiceProvider.GetRequiredService<NpgsqlDataSource>();
-
-    await using var connection = await dataSource.OpenConnectionAsync();
-
-    var exists = await connection.ExecuteScalarAsync<bool>(
-        """
-        SELECT EXISTS (
-            SELECT FROM information_schema.tables 
-            WHERE table_schema = 'public' 
-            AND table_name = 'users'
-        );
-        """
-    );
-
-    if (!exists)
-    {
-        await connection.ExecuteAsync(
-            """
-            CREATE TABLE users (
-                id SERIAL PRIMARY KEY,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL,
-                date_of_birth TIMESTAMP NOT NULL
-            );
-            """
-        );
-    }
-
-}
-#endregion
